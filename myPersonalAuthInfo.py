dfa6dd9b0af54bb3c53aeffa565fafed021b24a1
netcoredor <number6@ortizonline.com>
2021-02-22 11:36:26 -0500

    Initial Upload

---
+++ myPersonalAuthInfo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# This file contains all the necessary parameters for pushover.net, Kismet and Ubiquiti Unifi Controller
+
+
+#Kismet Wireless Config
+kismetUsername = "kismetuserhere"
+kismetPassword = "kismetpasswordhere"
+kismetServer = "kismetserveriphere"
+
+
+# Pushover.net Config
+pushoverServer = "api.pushover.net"
+pushoverToken = "pushoverTokenHere"
+pushoverUser = "pushoverUserTokenHere"
+pushoverDevice = "pushoverDeviceNameHere"
